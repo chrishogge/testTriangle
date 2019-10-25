5c9efae71a5f9827fd60d32ca14450c4f6367457
chris.hogge <chogge@ucvts.org>
2019-10-25 14:05:32 -0400

    Final

--- src/TestTriangle.java
+++ src/TestTriangle.java
@@ -1,4 +1,4 @@
-mport java.util.Scanner;
+import java.util.Scanner;
 
 public class TestTriangle {
 
@@ -6,21 +6,21 @@ public class TestTriangle {
 
     public static void main(String[] args) {
         System.out.print("\nEnter a base: ");
-        double triBase = next.Double();
+        double triBase = in.nextDouble();
         System.out.print("Enter a height: ");
-        double triHeight = next.Double();
-        System.out.print("\nEnter a hypotenuse: ");
-        double triHypo = next.Double();
+        double triHeight = in.nextDouble();
+        System.out.print("Enter a hypotenuse: ");
+        double triHypo = in.nextDouble();
 
         double aSqBsq = Math.pow(triBase,2) + Math.pow(triHeight,2);
         double cSq = Math.pow(triHypo,2);
 
         if(aSqBsq == cSq){
-            System.out.print("\nYes, that's a right triangle!");
+            System.out.print("\nYes, that's a right triangle!\n");
         }else if(aSqBsq != cSq){
-            System.out.print("\nNope...not a right triangle.");
+            System.out.print("\nNope...not a right triangle.\n");
         }else{
-            System.out.print("\nNope...not a right triangle.");
+            System.out.print("\nNope...not a right triangle.\n");
         }
     }
 }
